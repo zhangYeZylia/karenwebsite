89e6eb4d48ff550d8e78e2ce6aece7fb21442da0
zhangYeZylia <18249232705@163.com>
2019-02-19 15:01:52 +0800

    Update UserService.java

--- src/main/java/com.karen.ssm/service/UserService.java
+++ src/main/java/com.karen.ssm/service/UserService.java
@@ -2,11 +2,6 @@ package com.karen.ssm.service;
 
 import com.karen.ssm.model.User;
 
-/**
- * @author xsansan
- * @date 2018年8月8日
- * Description:
- */
 public interface UserService {
     User selectUserById(Integer userId);
 }
