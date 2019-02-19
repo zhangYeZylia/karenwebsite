f5d92b8283aacf916cd955a94d55249b517f88c3
zhangYeZylia <18249232705@163.com>
2019-02-19 15:01:00 +0800

    Update UserDao.java

--- src/main/java/com.karen.ssm/dao/UserDao.java
+++ src/main/java/com.karen.ssm/dao/UserDao.java
@@ -2,11 +2,6 @@ package com.karen.ssm.dao;
 
 import com.karen.ssm.model.User;
 
-/**
- * @author xsansan
- * @date 2018年8月8日
- * Description:
- */
 public interface UserDao {
     public User selectUserById(Integer userId);
 
