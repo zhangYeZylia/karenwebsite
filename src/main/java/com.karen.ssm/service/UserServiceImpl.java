73d76e12beb0b35a70720edcb5a6eb76f712de14
zhangYeZylia <18249232705@163.com>
2019-02-19 15:02:14 +0800

    Update UserServiceImpl.java

--- src/main/java/com.karen.ssm/service/UserServiceImpl.java
+++ src/main/java/com.karen.ssm/service/UserServiceImpl.java
@@ -6,11 +6,6 @@ import org.springframework.stereotype.Service;
 import com.karen.ssm.dao.UserDao;
 import com.karen.ssm.model.User;
 
-/**
- * @author xsansan
- * @date 2018年8月8日
- * Description:
- */
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
